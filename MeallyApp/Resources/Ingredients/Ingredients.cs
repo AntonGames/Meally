b2f25b85fbfb33c81d0bb59c1b06723f45705686
Gustas <loggustas@gmail.com>
2022-09-25 22:53:36 +0300

    Added enum of ingredients.

---
+++ MeallyApp/Resources/Ingredients/Ingredients.cs
@@ -0,0 +1,36 @@
+﻿
+namespace MeallyApp.Resources.Ingredients
+{
+    public enum Ingredients
+    {
+        Spaghetti = 0,
+        Butter = 1,
+        Garlic = 2,
+        Cheese = 3,
+        Pepper = 4,
+        CasterSugar = 5,
+        Flour = 6,
+        Eggs = 7,
+        Lemon = 8,
+        VanillaEssence = 9,
+        LemonCurd = 10,
+        OliveOil = 11,
+        Bacon = 12,
+        Onion = 13,
+        Celery = 14,
+        Carrot = 15,
+        Beef = 16,
+        Tomatoes = 17,
+        Honey = 18,
+        LasagneSheets = 19,
+        Mozzarella = 20,
+        Parmesan = 21,
+        CremeFraiche = 22,
+        Milk = 23,
+        Salt = 24,
+        Cucumber = 25,
+        Coriander = 26,
+        Potatoes = 27,
+        Paprika = 28
+    }
+}
