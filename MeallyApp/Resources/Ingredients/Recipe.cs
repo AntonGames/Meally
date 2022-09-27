1ce5cc3ed8b3f44c238896c42d7cf92d55a06665
Gustas <loggustas@gmail.com>
2022-09-27 19:31:50 +0300

    Added User and Recipe classes

---
+++ MeallyApp/Resources/Ingredients/Recipe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeallyApp.Resources.Ingredients
+{
+    public class Recipe
+    {
+        public string Name { get; }
+        
+        private List<Ingredient> ingredients;
+
+        public List<Ingredient> Ingredients { get { return ingredients; } } 
+
+        public string RecipeInstructions { get; }
+
+        public Recipe(string name, List<Ingredient> ingredients, string recipeInstructions)
+        {
+            Name = name;
+            this.ingredients = ingredients;
+            RecipeInstructions = recipeInstructions;
+        }
+    }
+}
---
+++ MeallyApp/UserData/User.cs
@@ -0,0 +1,21 @@
+﻿using MeallyApp.Resources.Ingredients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeallyApp.UserData
+{
+    public class User
+    {
+        private List<Ingredient> inventory;
+
+        public List<Ingredient> Inventory { get { return inventory; } }
+
+        public User(List<Ingredient> inventory)
+        {
+            this.inventory = inventory;
+        }
+    }
+}
