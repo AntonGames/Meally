2a2445b3bb434c5a63ac4857b9ea5a50d1c3a403
Gustas <loggustas@gmail.com>
2022-10-11 17:00:07 +0300

    Implemented IComparable interface for requirements.

--- MeallyApp/Resources/Ingredients/Recipe.cs
+++ MeallyApp/Resources/Ingredients/Recipe.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MeallyApp.Resources.Ingredients
 {
-    public class Recipe
+    public class Recipe : IComparable<Recipe>
     {
         public string Name { get; set; }
 
@@ -17,11 +17,28 @@ namespace MeallyApp.Resources.Ingredients
 
         public double Compatibility { get; set; } = 0;
 
-        public Recipe(string name, List<Ingredient> ingredients, string recipeInstructions)
+        public Recipe(string name, List<Ingredient> ingredients, string recipeInstructions, string recipeImage = "place_holder.png")
         {
             Name = name;
             this.Ingredients = ingredients;
             RecipeInstructions = recipeInstructions;
+        }
+
+        // 10. IComparable interface 
+        public int CompareTo(Recipe other)
+        {
+            if (this.Compatibility > other.Compatibility)
+            {
+                return -1;
+            }
+            else if (this.Compatibility < other.Compatibility)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
