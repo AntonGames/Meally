c26d1e5a025137d8c2e4a0ba90b7900c2dfbe745
Laurynas Gailius <lauris.gailius@gmail.com>
2022-10-24 09:06:51 +0300

    added converter to fix new lines

---
+++ MeallyApp/Resources/Converters/InstructionsToXAMLConverter.cs
@@ -0,0 +1,33 @@
+﻿using Android.Text.Method;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeallyApp.Resources.Converters
+{
+    internal class InstructionsToXAMLConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            //replace \n and \r as &#x0a;
+
+            value = value.ToString().Replace("\r\n", Environment.NewLine);
+            value = value.ToString().Replace("\n", Environment.NewLine);
+            value = value.ToString().Replace("\r", Environment.NewLine);
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
