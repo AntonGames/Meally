db673be6796d751101653bccdae671f6c17bc5da
Gustas <loggustas@gmail.com>
2022-11-10 19:29:12 +0200

    Added and implemented a class with generic methods.

---
+++ MeallyApp/Resources/Converters/IEnumerableHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace MeallyApp.Resources.Converters
+{
+    public static class IEnumerableHelpers
+    {
+        public static ObservableCollection<T> ConvertToObservableCollection<T>(IEnumerable<T> collection)
+        {
+            return new ObservableCollection<T>(collection);
+        }
+
+        public static IEnumerable<T> Swap<T>(IEnumerable<T> collection, int index1, int index2)
+        {
+            if (index1 < 0 || index1 >= collection.Count())
+            {
+                throw new ArgumentOutOfRangeException(nameof(index1));
+            }
+
+            if (index2 < 0 || index2 >= collection.Count())
+            {
+                throw new ArgumentOutOfRangeException(nameof(index2));
+            }
+
+            T[] collectionAsAnArray = collection.ToArray();
+            T tmp = collectionAsAnArray[index1];
+            collectionAsAnArray[index1] = collectionAsAnArray[index2];
+            collectionAsAnArray[index2] = tmp;
+
+            return collectionAsAnArray;
+        }
+    }
+}
