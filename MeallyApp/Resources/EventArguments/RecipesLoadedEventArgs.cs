a73bf8f732af43d82437fa972e1c747671709104
Gustas <loggustas@gmail.com>
2022-11-11 14:44:11 +0200

    Added EventArgs folder and argument class for recipe loaded event.

---
+++ MeallyApp/Resources/EventArguments/RecipesLoadedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeallyApp.Resources.EventArguments
+{
+    public class RecipesLoadedEventArgs : EventArgs
+    {
+        public string message;
+    }
+}
