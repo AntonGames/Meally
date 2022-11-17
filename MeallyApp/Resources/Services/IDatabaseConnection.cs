e2145431626712985639cba5d991a2af06347722
Gustas <loggustas@gmail.com>
2022-11-17 16:16:48 +0200

    IDatabaseConnection and it's implementation.

---
+++ MeallyApp/Resources/Services/IDatabaseConnection.cs
@@ -0,0 +1,18 @@
+﻿using MeallyApp.Resources.EventArguments;
+using MeallyApp.Resources.Ingredients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeallyApp.Resources.Services
+{
+    public interface IDatabaseConnection
+    {
+        public Task GetDBAsync();
+        public List<Recipe> GetRecipeList();
+
+        public event EventHandler<RecipesLoadedEventArgs> RecipesLoaded;
+    }
+}
