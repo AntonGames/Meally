76a9a48dc40db5342396036f7d0848e806c3fc8f
Gustas <loggustas@gmail.com>
2022-11-17 16:27:38 +0200

    Modified structure and added dependecy injection.

--- MeallyApp/FilterPage.xaml.cs
+++ MeallyApp/FilterPage.xaml.cs
@@ -1,9 +1,7 @@
 using MeallyApp.UserData;
-using MeallyApp.Resources;
 using MeallyApp.Resources.Services;
 using MeallyApp.Resources.ViewIngredients;
 using MeallyApp.Resources.ExceptionHandling;
-using Microsoft.Maui.Controls.Shapes;
 using MeallyApp.Resources.EventArguments;
 
 namespace MeallyApp;
@@ -16,32 +14,40 @@ public partial class FilterPage : ContentPage
 
     // 4. Delegate usage
 
-    private ExceptionDelegate action; 
+    private ExceptionDelegate action;
 
-    public FilterPage(RecipeViewModel viewModel)
+    private IExceptionLogger logger;
+    private IDatabaseConnection databaseConnection;
+    private IRecipeHandler recipeHandler;
+
+    public FilterPage(RecipeViewModel viewModel, IExceptionLogger logger, IDatabaseConnection connection, IRecipeHandler recipeHandler)
     {
         InitializeComponent();
         BindingContext = viewModel;
         ViewModel = viewModel;
 
+        this.logger = logger;
+        this.databaseConnection = connection;
+        this.recipeHandler = recipeHandler;
+
         //Subscribing to custom event
-        ExceptionLogger.exceptionAddedToFile += delegate (string message)  // added event handler method to a custom event
+        logger.exceptionAddedToFile += delegate (string message)  // added event handler method to a custom event
         {
             System.Diagnostics.Debug.WriteLine(message);
         };
 
         //Subscribing to standart event
-        RecipeHandler.RecipesLoaded += delegate (object sender, RecipesLoadedEventArgs arguments)  // added even handler method to a standart event
+        databaseConnection.RecipesLoaded += delegate (object sender, RecipesLoadedEventArgs arguments)  // added even handler method to a standart event
         {
             System.Diagnostics.Debug.WriteLine("Standart event:  " + arguments.message);
         };
-        action = ExceptionLogger.ClearLog;
+        action = logger.ClearLog;
         action();
     }
 
     private void ExceptionLogButton_OnClicked(object sender, EventArgs e)
     {
-        action = ExceptionLogger.ReadFromLog;
+        action = logger.ReadFromLog;
         action();
     }
 
@@ -50,9 +56,9 @@ public partial class FilterPage : ContentPage
         if (!Loader.IsRunning)
         {
             Loader.IsRunning = true;
-            await RecipeHandler.GetDBAsync();
-            RecipeHandler.SetComp(User.inventory);
-            RecipeHandler.OrderDB();
+            await recipeHandler.GetRecipesFromDB();
+            recipeHandler.SetComp(User.inventory);
+            recipeHandler.OrderDB();
             Loader.IsRunning = false;
             ViewModel.GetRecipesCommand.Execute(this);
         }
